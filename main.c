9eb6e9cee66a9d3a484b1d16286be49f23bde638
ShoReagan <shoreaganw@gmail.com>
2023-04-01 11:12:46 -0500

    setup

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
+#include "clock.h"
+#include "gpio.h"
+#include "wait.h"
+#include "nvic.h"
+#include "uart0.h"
+#include "adc.h"
+
+int main()
+{
+
+}
